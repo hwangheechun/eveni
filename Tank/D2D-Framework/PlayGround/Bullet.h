d166d54138358c55b04ba4858a067d6dbd7880e5
hwangheechun <heechun3525@gmail.com>
2025-01-12 15:43:27 +0900

    1차 시도_체력바 그리기&충돌

---
+++ Tank/D2D-Framework/PlayGround/Bullet.h
@@ -0,0 +1,69 @@
+﻿#pragma once
+#include "GameObject.h"
+
+class Bullet : public GameObject
+{
+public:
+	Bullet();
+	virtual ~Bullet();
+
+	void Init() override;
+	void Release() override;
+	void Update() override;
+	void Render() override;
+
+	void Move(Vector2 moveDirection, float speed);
+	void MoveAngle(float speed);
+
+	class Player* _player;
+	bool _isInit;
+	Vector2 _direction;
+
+	int countCollide;
+};
+
+class ReinforcedBullet : public GameObject
+{
+public:
+	ReinforcedBullet();
+	virtual ~ReinforcedBullet();
+
+	void Init() override;
+	void Release() override;
+	void Update() override;
+	void Render() override;
+
+	void Move(Vector2 moveDirection, float speed);
+	void MoveAngle(float speed);
+
+	class Player* _player;
+	bool _isInit;
+	Vector2 _direction;
+
+	int countCollide;
+	bool _isFire;
+};
+
+class Shot : public GameObject
+{
+public:
+	Shot();
+	virtual ~Shot();
+
+	void Init() override;
+	void Release() override;
+	void Update() override;
+	void Render() override;
+
+	void Move(Vector2 moveDirection, float speed);
+	void MoveAngle(float speed);
+
+	class Player* _player;
+	bool _isInit;
+	Vector2 _direction;
+
+	int countCollide;
+
+	void SetType(float a);
+};
+
